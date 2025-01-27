6518eca2616cb6ee77cf264187cb018242d299a7
Samiksha220577 <145250111+Samiksha220577@users.noreply.github.com>
2025-01-27 21:52:31 +0530

    27-01-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,24 +1,2 @@
-from typing import List
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-# only add open paranthesis if open<n
-# only add a closing paranthesis if closed<open
-#valid IIF open==close==n
-        stack=[]
-        res=[]
-        def backtrack(openN,closedN):
-            if openN==closedN==n:
-                res.append("".join(stack))
-                return
-            if openN<n:
-                stack.append("(")
-                backtrack(openN+1,closedN)
-                stack.pop()
-            if closedN<openN:
-                stack.append(")")
-                backtrack(openN,closedN+1)
-                stack.pop()
-        backtrack(0,0)
-        return res
-sol = Solution()
-print(sol.generateParenthesis(3))
+    def divide(self, dividend: int, divisor: int) -> int:
