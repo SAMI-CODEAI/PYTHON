044338b26b86be003a627bd62cbc12a71858bacf
Samiksha220577 <145250111+Samiksha220577@users.noreply.github.com>
2025-01-29 23:49:58 +0530

    28-01-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,2 +1,27 @@
+from openpyxl.styles.builtins import output
+
+
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
+        d=abs(dividend)
+        dv=abs(divisor)
+        output=0
+
+        while d>=dv:
+            temp=dv
+            mul=1
+            while d>=temp:
+                d-=temp
+                output+=mul
+                mul+=mul
+                temp+=temp
+        if(dividend<0 and divisor>=0) and (divisor<0 and dividend>=0) :
+            output=-output
+
+        return min(2147483647 ,max(-2147483647,output))
+
+
+
+
+sol=Solution()
+print(sol.divide(100000,3))
