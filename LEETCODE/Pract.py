d0c29433343be338586903b53e2e05398f4ebf64
Samiksha220577 <145250111+Samiksha220577@users.noreply.github.com>
2025-01-30 19:19:26 +0530

    29-01-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,27 +1,15 @@
-from openpyxl.styles.builtins import output
-
-
 class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
-        d=abs(dividend)
-        dv=abs(divisor)
-        output=0
-
-        while d>=dv:
-            temp=dv
-            mul=1
-            while d>=temp:
-                d-=temp
-                output+=mul
-                mul+=mul
-                temp+=temp
-        if(dividend<0 and divisor>=0) and (divisor<0 and dividend>=0) :
-            output=-output
-
-        return min(2147483647 ,max(-2147483647,output))
-
-
-
-
-sol=Solution()
-print(sol.divide(100000,3))
+    def reverseVowels(self, s: str) -> str:
+        s=list(s)
+        vowels=set('aeiouAEIOU')
+        i,j=0,len(s)-1
+        while i<j:
+            if s[i] not in vowels:
+                i+=1
+            elif s[j] not in vowels:
+                j-=1
+            else:
+                s[i],s[j]=s[j],s[i]
+                i+=1
+                j-=1
+        return''.join(s)
