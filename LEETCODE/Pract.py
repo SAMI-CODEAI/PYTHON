9c56b3ebf594762d3508757b25b62aed9ea27df7
Samiksha220577 <145250111+Samiksha220577@users.noreply.github.com>
2025-01-31 22:02:02 +0530

    31-01-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,15 +1,14 @@
+from panel.interact import empty
+
+
 class Solution:
-    def reverseVowels(self, s: str) -> str:
-        s=list(s)
-        vowels=set('aeiouAEIOU')
-        i,j=0,len(s)-1
-        while i<j:
-            if s[i] not in vowels:
-                i+=1
-            elif s[j] not in vowels:
-                j-=1
-            else:
-                s[i],s[j]=s[j],s[i]
-                i+=1
-                j-=1
-        return''.join(s)
+    def isSubsequence(self, s: str, t: str) -> bool:
+        for i in s:
+            if i in t:
+              s.rstrip(i)
+        if s is=='':
+            return True
+        else:
+            return False
+sol = Solution()
+print(sol.isSubsequence("abc", "ahbgdc"))
