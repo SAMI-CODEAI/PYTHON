3bd3e22d78a4a2781c8cfbe15f01b6f51104b44a
SAMI-CODEAI <sami.codeai@gmail.com>
2025-02-03 23:03:40 +0530

    3-0-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,14 +1,2 @@
-from panel.interact import empty
-
-
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        for i in s:
-            if i in t:
-              s.rstrip(i)
-        if s is=='':
-            return True
-        else:
-            return False
-sol = Solution()
-print(sol.isSubsequence("abc", "ahbgdc"))
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
