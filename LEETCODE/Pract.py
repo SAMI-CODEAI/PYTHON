eb10ac73f7936ba977b538f7242e0798b04692a7
SAMI-CODEAI <sami.codeai@gmail.com>
2025-02-04 22:22:31 +0530

    04-02-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,2 +1,29 @@
+
+
 class Solution:
-    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+    def countPrimes(self, n: int) -> int:
+        # 0 and 1 are not primes
+        if n==0 or n==1:return 0
+        primes=[1]*n
+        primes[0]=0
+        primes[1]=0
+
+        i=2
+        while i<n:
+            temp=i
+            if primes[i]:
+                temp+=i
+                while temp<n:
+                    primes[temp]=0
+                    temp+=i
+            i+=1
+
+
+        return sum(primes)
+
+
+
+
+
+sol=Solution()
+print(sol.countPrimes(30))
