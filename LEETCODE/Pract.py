0290eec191c353950ee2b16836e40428bc42269d
SAMI-CODEAI <sami.codeai@gmail.com>
2025-02-11 00:20:57 +0530

    10-02-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,29 +1,23 @@
+from typing import List
 
 
 class Solution:
-    def countPrimes(self, n: int) -> int:
-        # 0 and 1 are not primes
-        if n==0 or n==1:return 0
-        primes=[1]*n
-        primes[0]=0
-        primes[1]=0
+    def compress(self, chars: List[str]) -> int:
+        r,i=0,0
+        while(i<len(chars)):
+            currChar=chars[i]
+            currOcc=0
+            while((i<len(chars)) and (chars[i]==currChar)):
+                currOcc+=1
+                i+=1
+            chars[r]=currChar
+            r+=1
+            if(currOcc>1):
+                currOccStr=str(currOcc)
+                for j in range(len(currOccStr)):
+                    chars[r]=currOccStr[j]
+                    r+=1
 
-        i=2
-        while i<n:
-            temp=i
-            if primes[i]:
-                temp+=i
-                while temp<n:
-                    primes[temp]=0
-                    temp+=i
-            i+=1
-
-
-        return sum(primes)
+        return r
 
 
-
-
-
-sol=Solution()
-print(sol.countPrimes(30))
