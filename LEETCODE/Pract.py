2ec6dfe1f55dccc70a246f5f9c8f1f8d03979d22
sami-codeai <sami.codeai@gmail.com>
2025-03-13 16:35:22 +0530

    10-02-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,23 +1,24 @@
-from typing import List
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Data steps during the sorting process
+steps = [
+    [3, 5, 9, 7, 1, 4, 6, 8, 2],  # Initial sequence
+    [1, 2, 3, 5, 9, 7, 4, 6, 8],  # After pivot = 2
+    [1, 2, 3, 5, 7, 4, 6, 8, 9],  # After pivot = 8
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],  # Fully sorted
+]
 
-class Solution:
-    def compress(self, chars: List[str]) -> int:
-        r,i=0,0
-        while(i<len(chars)):
-            currChar=chars[i]
-            currOcc=0
-            while((i<len(chars)) and (chars[i]==currChar)):
-                currOcc+=1
-                i+=1
-            chars[r]=currChar
-            r+=1
-            if(currOcc>1):
-                currOccStr=str(currOcc)
-                for j in range(len(currOccStr)):
-                    chars[r]=currOccStr[j]
-                    r+=1
+# Stack the data for visualization
+steps_stacked = np.array(steps)
 
-        return r
+# Visualize the data as a single bar graph
+plt.figure(figsize=(10, 6))
+for i in range(steps_stacked.shape[1]):
+    plt.bar(i, steps_stacked[:, i], color=plt.cm.viridis(i / steps_stacked.shape[1]))
 
-
+# Adding labels and title
+plt.title("Quicksort Simulation: Progression of Sorting")
+plt.xlabel("Array Index")
+plt.ylabel("Value")
+plt.show()
