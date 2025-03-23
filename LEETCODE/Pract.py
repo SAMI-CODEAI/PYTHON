5a042de81ef24a15efb4753b67db2aec47171687
sami-codeai <sami.codeai@gmail.com>
2025-03-23 22:37:16 +0530

    23-03-25-1

--- LEETCODE/Pract.py
+++ LEETCODE/Pract.py
@@ -1,24 +1,18 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from collections import deque
 
-# Data steps during the sorting process
-steps = [
-    [3, 5, 9, 7, 1, 4, 6, 8, 2],  # Initial sequence
-    [1, 2, 3, 5, 9, 7, 4, 6, 8],  # After pivot = 2
-    [1, 2, 3, 5, 7, 4, 6, 8, 9],  # After pivot = 8
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],  # Fully sorted
-]
 
-# Stack the data for visualization
-steps_stacked = np.array(steps)
+class RecentCounter:
 
-# Visualize the data as a single bar graph
-plt.figure(figsize=(10, 6))
-for i in range(steps_stacked.shape[1]):
-    plt.bar(i, steps_stacked[:, i], color=plt.cm.viridis(i / steps_stacked.shape[1]))
-
-# Adding labels and title
-plt.title("Quicksort Simulation: Progression of Sorting")
-plt.xlabel("Array Index")
-plt.ylabel("Value")
-plt.show()
+    def __init__(self):
+        self.q=deque()
+        self.cnt=0
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        self.cnt+=1
+        while self.q and self.q[0]<t-3000:
+            self.q.popleft()
+            self.cnt-=1
+        return self.cnt
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
